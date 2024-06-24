27f5b2cdc64bdb273422007935cac975bbf58e5a
André Sousa <107645540+andrelzsousa@users.noreply.github.com>
2024-06-23 23:35:32 -0300

    spotify auth

---
+++ src/app/(authenticated)/creator/page.tsx
@@ -0,0 +1,9 @@
+const CreatePage = () => {
+    return (
+        <div>
+        <h1>Page</h1>
+        </div>
+    );
+}
+
+export default CreatePage;
---
+++ src/app/(authenticated)/layout.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { storageGet } from "@/services/localStorage";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const AuthenticatedLayout = ({ children }: { children: React.ReactNode }) => {
+    const router = useRouter();
+    const token = storageGet("token");
+    
+    useEffect(() => {
+        if(!token) {
+            router.push("/");
+        }
+    }, [token, router])
+
+    return (
+        <div>{children}</div>
+    );
+}
+
+export default AuthenticatedLayout;
